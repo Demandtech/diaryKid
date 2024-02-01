753f7476ee25daca34d518e1a5d18ff1ee51bb3c
demandtech <demandwork247@gmail.com>
2024-02-01 22:31:35 +0100

    font-size change

--- src/components/Tokenomic.jsx
+++ src/components/Tokenomic.jsx
@@ -6,20 +6,20 @@ export default function Tokenomic() {
       <h2 className='text-3xl mb-3'>Tokenomic</h2>
       <div className='flex flex-col lg:flex-row flex-wrap gap-5 pb-24'>
         <div className='text-xl flex item-center justify-between border-2 border-white rounded-md px-4 py-3 min-w-[300px] flex-1'>
-          <p className='text-xl'>Symbol</p>
-          <p className='text-white/70'>$KID</p>
+          <h4 className='text-xl'>Symbol</h4>
+          <h4 className='text-white/70'>$KID</h4>
         </div>
         <div className='text-xl flex item-center justify-between border-2 border-white rounded-md px-4 py-3 min-w-[300px] flex-1'>
-          <p>Tax</p>
-          <p className='text-white/70'>0/0</p>
+          <h4>Tax</h4>
+          <h4 className='text-white/70'>0/0</h4>
         </div>
         <div className='text-xl flex item-center justify-between border-2 border-white rounded-md px-4 py-3 min-w-[300px] flex-1'>
-          <p>Liquidity</p>
-          <p className='text-white/70'>Burned Forever</p>
+          <h4>Liquidity</h4>
+          <h4 className='text-white/70'>Burned Forever</h4>
         </div>
         <div className='text-xl flex item-center justify-between border-2 border-white rounded-md px-4 py-3 flex-wrap min-w-[300px] w-full'>
-          <p>Token Address</p>
-          <p className='text-white/70'>0x0122354444444444444</p>
+          <h4>Token Address</h4>
+          <h4 className='text-white/70'>0x0122354444444444444</h4>
         </div>
       </div>
       <div className='flex justify-between  text-white items-center pb-10'>
